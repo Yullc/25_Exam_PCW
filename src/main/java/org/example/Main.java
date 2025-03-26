c54e3cb37d56190ec6f18cd9e783b8c6dc1b7b57
Yullc <apanan0910@gmail.com>
2025-03-26 17:21:29 +0900

    3번 문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,18 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 정수로된 변수 n을 사용자가 입력
-        int m = 1; // 정수 m변수에 1을 넣음
-        for (int i = n; i > 0; i--) { //for문을 도는데 i=n부터 1씩 감소도되록 설정
-            for (int j = 0; j < n - i; j++) { // j는 0부터 n-i까지 즉 사용자가 입력한 값에서 n-1..n-2..n-3..씩 빼면서 빈 공간을 출력
-                System.out.print(" ");
-            }
-            for (int k = n; k - m >= 0; k--) { // k는 n부터 반복문을 도는데 사용자가 입력한 값 -m만큼돌음 그러면 처음에는 4 두번째는 3...
-                System.out.print(k - (m - 1) + " ");// k에서 m-1을 뺌 처음에는 k-2-1 , k-3-1 k-4-1 이런식으로 m이 증가되면서 빈공간을 출력
-            }
-            System.out.println(); // 줄바꿈
-            m++; // m은 1씩 증가
+        int arr[] = new int[12]; //길이가 12인 배열 객체 생성 왜냐하면 피보나치를 계산할 때 현재자릿수에서 +2한 자리까지 더해서 구할 생각이기 떄문이다.
+        arr[0]=0; // 초기값으로 0번째 배열에 0을 넣고
+        arr[1] =1;// 초기값으로 1번째 배열에 1을 넣음
+
+        for(int i=0; i<arr.length-2; i++){ // for문으로 arr배열길이의 -2 만큼 배열을 돈다. 왜냐하면 출력되야 하는 배열의 길이는 10이므로 -2를 해준다
+            arr[i+2] = arr[i]+arr[i+1]; // 그러면 현재 자리에서 2칸 증가된 값을 구하는 방법은 현재의 자리값과 다음 자리값을 더한값이 +2가 된 자리값이다.
+            System.out.print(" "+arr[i]);// 현재 자리값 출력
         }
     }
 }

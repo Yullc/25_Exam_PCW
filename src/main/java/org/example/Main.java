6ad776172ecea23fe2bb8d67c2d244be3c413264
Yullc <apanan0910@gmail.com>
2025-10-28 11:37:36 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,7 @@ public class Main {
         for(int i=0; i<arr.length-2; i++){ // for문으로 arr배열길이의 -2 만큼 배열을 돈다. 왜냐하면 출력되야 하는 배열의 길이는 10이므로 -2를 해준다
             arr[i+2] = arr[i]+arr[i+1]; // 그러면 현재 자리에서 2칸 증가된 값을 구하는 방법은 현재의 자리값과 다음 자리값을 더한값이 +2가 된 자리값이다.
             System.out.print(" "+arr[i]);// 현재 자리값 출력
+            System.out.print(" "+arr[i]);// 현재 자리값 출력
         }
     }
 }
